609f079f03109f216acad02e030271f5b5361a27
olvlada <ol.vlada.zp@gmail.com>
2017-05-31 09:05:31 +0300

    add export for custom images

--- project/api.py
+++ project/api.py
@@ -29,6 +29,8 @@ class ExportToJpgView(APIView):
 
     def post(self, request, *args, **kwargs):
         result_data = json.loads(request.body)
+        for result_item in result_data:
+            result_item['custom_domain_image'] = result_item.get('customDomainImage', None)
         template = loader.get_template('export.html')
         context = Context({'data': result_data})
         file_code = random.randint(0, 999999999)
